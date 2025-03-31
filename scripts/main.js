5d62e16efab5e328fb54195878e8f6fdef7b1be6
Ann Oleynik <anna.oleynik.08.07.2001@gmail.com>
2025-03-31 12:22:45 +0300

    commit-5

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import Header from './Header.js'
 import TabsCollection from './Tabs.js'
 import VideoPlayerCollection from './VideoPlayer.js'
 import ExandableContentCollection from './ExpandableContent.js'
+import InputMaskCollection from './InputMask.js'
 
 new Header()
 new TabsCollection()
 new VideoPlayerCollection()
 new ExandableContentCollection()
+new InputMaskCollection()
